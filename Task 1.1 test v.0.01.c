fd0b00202f69c33c144ca87ed2c6ca33d44b1122
Code_marine_271 <32020828+HCL-271@users.noreply.github.com>
2019-11-16 14:43:01 +0300

    Added new test 0.01
    Добавил тест. В нем проверяется возможность введения значений, которые по логике не могут существовать (создание списка длиной -1, удаление первых -10 элементов и т.д)

--- Task 1.1 test v.0.01.c
+++ Task 1.1 test v.0.01.c
@@ -1,15 +1,34 @@
-#include <stdio.h>        //Забей на тесты, я не знаю, как должны работать эти функции.
+#include <stdio.h>  
 int Sanya(void) //Classic
 {
 	return 100;
 }
+/*
+*Смысл данного теста прост: я пытаюсь создать список с неположительным числом элементов а потом удалить 10 элементов и -10 элементов.
+* Я хочу чтобы либо этот код работал, либо отвечал мне, где я не правильно ввел данные.
+*У дачи.
+*/
 int main(void)                  // 
-{                
-	int a = 1;
-	list *lst1 = list_new();
-	insert(lst1, a);
-	print(lst1);
-	remove(lst1, a);
-	list_delete(lst1);
+{
+	struct list *rt = NULL;
+	rt = create_root();
+	struct list *un = NULL;
 
-}	
+	create_bare_list(rt, -2);
+	insert_value(rt, 3);
+	print_list(rt);
+	print_list(un);
+	delete_unit(un, 10);
+	delete_unit(rt, -10);
+	delete_list(un);
+		
+	struct list *Dio = NULL;
+	create_bare_list(Dio, 6);
+	insert_value(Dio, 5);//вводим значения -1; 2; 0; 10; -10
+	delete_unit(Dio, 10);
+	delete_unit(Dio, 7);
+	print_list(Dio);
+
+	return 0;
+
+}
