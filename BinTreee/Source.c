f6b019441f5dfdf5af8a02e63eea7a409bd2abe8
Alexander <54119300+SuperCrabLover@users.noreply.github.com>
2019-12-09 00:03:16 +0300

    Create Source.c
    Has brains for functions in header.h

---
+++ BinTreee/Source.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "BinTree.h"
+#define MALLOC_ERROR -1
+
+node* create_node(int x) {
+	node* n = (node *)malloc(sizeof(node));
+	if (NULL == n)
+	{
+		printf("create_node() error: MALLOC_ERROR");
+		return MALLOC_ERROR;
+	}
+	n->value = x;
+	n->left = n->right = NULL;
+	return n;
+}
+
+node* insert_node(node* n, int x) {
+	if (n == NULL)
+		return create_node(x);
+	if (n->value > x)
+		n->left = insert_node(n->left, x);
+	else
+		n->right = insert_node(n->right, x);
+	return n;
+}
+
+node* tree_dfs(node* root)
+{		
+	if (NULL == root)
+	{
+		return;
+	}
+	printf("%d\n", root->value);
+	tree_bfs(root->left);
+	tree_bfs(root->right);
+}
+
+node* tree_bfs(node* root)
+{
+	static int counter = 0;
+	if (root == NULL)
+		return NULL;
+	printf("%d ", root->value);
+	if (counter % 2 == 0)
+	{
+		counter++;
+		tree_bfs(root->left);
+	}
+	counter = 1;
+	if (counter % 2 == 1)
+	{
+		counter++;
+		tree_bfs(root->right);
+	}
+}
+
+
+
+node* find_nodeparent(node* parent, node* root, int key) // find_nodeparent(NULL, NODE*, INT) returns NULL if he didn't find any
+{
+	if (root == NULL)
+	{
+		return NULL;
+	}
+	if (root->value > key)
+	{
+		return find_nodeparent(root, root->left, key);
+	}
+	if (root->value < key)
+	{
+		return find_nodeparent(root, root->right, key);
+	}
+	if (root->value == key)
+	{
+		return parent;
+	}
+}
+
+void delete_leaf(node* leafparent, node* leaf)
+{
+	if (leafparent == NULL)
+	{
+		printf("deleteleaf() error: leafparent is null\n");
+		return;
+	}
+	if (leafparent->left == leaf)
+	{
+		free(leaf);
+		leafparent->left = NULL;
+	}
+	if (leafparent->right == leaf)
+	{
+		leafparent->right = NULL;
+		free(leaf);
+	}
+}
+
+void delete_node(node* root, int key)
+{
+	node* deletingNode = find_node(root, key);
+	if (deletingNode == NULL)
+	{
+		printf("delete_node() error: deleting node\n");
+		return -1;
+	}
+	
+	if (deletingNode->left == NULL && deletingNode->right == NULL)
+	{
+		delete_leaf(find_nodeparent(ROOT_PARENT, root, deletingNode->value), deletingNode);
+		return;
+	}
+
+	if (deletingNode->left == NULL && NULL != deletingNode->right)
+	{
+		find_nodeparent(ROOT_PARENT, root, deletingNode->value)->right = deletingNode->right;
+		free(deletingNode);
+		return;
+	}
+	node* rightLeafParent = find_rightleafparent(ROOT_PARENT, deletingNode->left);
+	
+	if (rightLeafParent == NULL)
+	{
+		deletingNode->value = deletingNode->left->value;
+		free(deletingNode->left);
+		deletingNode->left = NULL;
+		return;
+	}
+
+	deletingNode->value = rightLeafParent->right->value;
+	free(rightLeafParent->right);
+	rightLeafParent->right = NULL;
+	return;
+}
+
+void print_node(node* n) {
+	if (n == NULL)
+		return -1;
+	print_node(n->left);
+	printf("%d ", n->value);
+	print_node(n->right);
+}
