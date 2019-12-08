015a903a0baca67020305b30a8d0943f512f4cce
gleb-mak <57875990+gleb-mak@users.noreply.github.com>
2019-12-09 02:44:14 +0300

    Update small_test.c
    add check del function and deinit function

--- Task 1.14/small_test.c
+++ Task 1.14/small_test.c
@@ -2,13 +2,17 @@
 
 int main(void)
 {
+	char s[30];
+	char ch = 0;
+	char *end = "end";
 	char *a = "Ivanov";
 	char *b = "Sidorov";
 	char *c = "Petrov";
 	char *d = "Makarov";
 	char *e = "Tkach";
 	char *f = "Ivanov";
-	unsigned long long a1 = 1234567, b1 = 7654321, c1 = 1357264, d1 = 2641357, e1 = 7531264, f1 = 1326547;
+	unsigned long long a1 = 1234567, b1 = 7654321, c1 = 1357264, d1 = 2641357, e1 = 7531264, f1 = 1326547, s1 = 0;
+	hash_map_init();
 	add(a, a1);
 	add(b, b1);
 	add(c, c1);
@@ -20,7 +24,15 @@ int main(void)
 		print_dl_list(hash_map[i]);
 		printf("\n");
 	}
-	print_dl_list(find("Ivanov"));
-	_getch();
+	printf("----------------------------------\n");
+	print_dl_list(find("Makarov"));
+	printf("\n----------------------------------\n");
+	del("Ivanov");
+	for (int i = 0; i < N; i++)
+	{
+		print_dl_list(hash_map[i]);
+		printf("\n");
+	}
+	hash_map_deinit();
 	return 0;
 }
