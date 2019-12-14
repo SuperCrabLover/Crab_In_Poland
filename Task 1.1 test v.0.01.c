223452bfbf5730d0336a3ed48cc7af2c0348220d
Дмитрий Коврижных <kovrizhnykh.diu@phystech.edu>
2019-12-14 19:09:23 +0300

    Update Task 1.1 test v.0.01.c
    И снова обновил тесты

--- Task 1.1 test v.0.01.c
+++ Task 1.1 test v.0.01.c
@@ -1,74 +1,113 @@
 ﻿/*
-* САНЯ ПРОЧТИ!!!!!
 * Test1.1.c
-* Данный тест проверяет работоспособность системы функций, созданной моим коллегой
-* В каждом закомиченном блоке, кроме первого содержится какой-то баг, который тебе нужно пофиксить. 
-* Прочел? Молодец! А теперь иди вниз скрипта и разкоммить return 0;  в конце, иначе у тебя скрипт не будет работать.
-* Good luck!
+* Brief task 1.1 testing
+* Created 14.11.2019
+* (c) MIPT 2019
 */
-#include <stdio.h> 
-int main(void)                 
-{	
-	//Простой тест. необходимый для демонстрации работы кода. Здесь всё работает
-	/*struct list *root1 = NULL;
+#include <stdio.h>
+#include <stdlib.h>
+void test1() //Базовый тест на работу всех сущесвующих функций
+{
+	int a;
+
+	a = (rand() % 15) + 1;
+	int b = rand() % a + 1;
+	int c = rand() % 10 + 1;
+	int d = rand() % 5 + 1;
+	struct list *root1 = NULL;
 	root1 = create_root();
-	create_bare_list(root1, 10);
-	insert_value(root1, 5);
+	create_bare_list(root1, a);
+	insert_value(root1, b);
+	struct list *rt2 = create_unit(c);
+	add_to_end(root1, rt2);
 	print_list(root1);
-	delete_unit(root1, 7);
-	delete_list(root1);*/
-	
-	
-	/*
+	delete_unit(root1, d);
+	delete_list(root1);
+
+	struct list *rt1 = NULL;
+	rt1 = create_root();
+	read_list(rt1, c + 3);
+	add_to_middle(rt1, c / 2 + 1);
+	find_unit(rt1, d);
+	print_list(rt1);
+	delete_list(rt1);
+
+	return 0;
+}
+
+void test2()//Ломает код
+{
+	struct list *un = NULL;//Как видишь, я могу удалить лист, а затем прочитать его и вызвать ошибку.
+	un = create_root();
+	printf("\n");
+	create_bare_list(un, 10);
+	insert_value(un, 5);
+	print_list(un);
+	delete_list(un);
+	print_list(un);
+}
+
+void test_1337_gachi_king_code_breaker()//Кто вообще будет читать эти тесты?
+{
 	struct list *Dio = NULL;
 	Dio = create_root();
 	create_bare_list(Dio, 6);
-	insert_value(Dio, 5);//вводим значения -1; 20000000000000000000000000000; 0; 10; -10, т.е заведомо большее значение и integer преобразует его в другое число
-	delete_unit(Dio, 10);//А здесь вводим 0.1 // если это некорректно, то напиши мне
+	insert_value(Dio, 5);
 	delete_unit(Dio, 600);//Здесь вводим значение 7. Ошибка в том, что я могу ввести число юнитов больше, чем длина списка. А затем сослаться на элемент вне списка. (Если длина списка 6, а я сошлюсь на 7 -ой)
 	find_unit(Dio, 100);// Здесь тоже вводим значение 7. Тот же самый баг.
 	print_list(Dio);
-	*/
-	/*
+}
+
+void test_jojo_ORA_ORA_ORA_code_destructor()
+{
 	struct list *ban = NULL;
 	ban = create_bare_list(ban, 6);
-	ban = create_bare_list(ban, 6);// Тут я хз, что не так. Но ошибку выдает
-	*/
-	/*
+	ban = create_bare_list(ban, 6);
+}
+
+void test_fixed_bug()
+{
 	struct list *Glad_Valakas = NULL;
 	Glad_Valakas = create_root();
 	Glad_Valakas = add_to_end(Glad_Valakas, NULL);
-	*/
+}
 
-	/*
-	struct list *posvyat_rt = NULL;
-	posvyat_rt = create_root();
-	create_bare_list(posvyat_rt, NULL);//Введи NULL, если это корректно для юзера
-
-	*/
-	/*
+void test_Moon_prism_give_me_power()
+{
 	struct list *ok_boomer = NULL;
 	ok_boomer = create_root();
 	create_bare_list(ok_boomer, 6);
 	insert_value(ok_boomer, 7);
 	print_list(ok_boomer);
-	*/
-	/*
-	struct list *ok_zoomer = NULL;
-	ok_zoomer = create_root();
-	create_bare_list(ok_zoomer, 6);
-	insert_value(ok_zoomer, NULL);//Здесь нужно снова ввести NULL
-	print_list(ok_zoomer);
-	*/
-/*
-	struct list *ok_doomer = NULL;
-	ok_doomer = create_root();
-	read_list(ok_doomer, NULL);//Cнова ввести NULL, если это корректно для юзера
-	read_list(ok_doomer, 5);
-	//Вопрос: куда исчез ввод остальных значений?
-	print_list(ok_doomer);
+}
+
+#include <stdio.h> 
+int main(void)
+{
+	test1();//Собственно тест с обещанным рандомом.
+
+	/* На самом деле этот рандом полная фигня, т.к из-за ввода из консоли мы не можем
+	*  создать 1000 списков по 1000000 элементов в каждом, чтобы проверить работоспособность
+	*  программы на больших числах.
+	*  Можно конечно создать файл с этими числами, но тогда любой юзер тоже должен иметь
+	*  такой файл с числами у себя на пк. В программу можно добавить кусок с созданием
+	*  кода, который создает файл. но это затратит слишком много времени, а баллов за это
+	*  не дают, так что обойдемся без этого.
+	*  P.S. В конце концов я не Подлесных, чтобы делать сверхжесткие тесты)
 	*/
 
+	test2();// Ломает код
+	test_1337_gachi_king_code_breaker();
+	// Эти два теста оставлены на исправление
 
-//return 0;
+	// Эти тесты нужны для демонстрации
+	test_jojo_ORA_ORA_ORA_code_destructor();
+	// Этот баг пофикшен, но я его оставлю как пример работы.
+	test_fixed_bug();
+	//Ещё один тест с исправленным багом
+	test_Moon_prism_give_me_power();
+	//И ещё один тест с исправленным багом
+
+	return 0;
+
 }
