cfca60e62d13ccd3f70868ae04942321d2302466
Poland-Strong <32020828+HCL-271@users.noreply.github.com>
2019-12-15 16:05:39 +0300

    Update common.h

--- Task 1.14/common.h
+++ Task 1.14/common.h
@@ -1,3 +1,10 @@
+/*
+ * File:  common.h 
+ * Brief: Definitions for header for task 1.14
+ * Author: Code squad 1337
+ * Created on 18.11.2019
+ * (c) MIPT 2019
+ */
 #define _CRT_SECURE_NO_WARNINGS
 #pragma once
 
