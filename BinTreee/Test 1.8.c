3d570f4270714b5a8b0a4ed389442fc86167f31e
Poland-Strong <32020828+HCL-271@users.noreply.github.com>
2019-12-15 17:23:36 +0300

    Update Test 1.8.c

--- BinTreee/Test 1.8.c
+++ BinTreee/Test 1.8.c
@@ -18,8 +18,6 @@ void test_1()
 	{
 		n2 = insert_node(n2, i);
 	}
-	print_node(n2);
-	printf("\n");
 	n2 = tree_dfs(n2);
 	
 }
@@ -35,7 +33,7 @@ void test_2()
 	{
 		n2 = insert_node(n2, i + (rand() % 10));
 	}
-	print_node(n2);
+
 	n2 = tree_bfs(n2);
 }
 
@@ -48,13 +46,10 @@ void test_3()
 		n2 = insert_node(n2, i + (rand() % 10));
 	}
 	n2 = insert_node(n2, 10);//Этот тест показывает наличие автоматической сортировки
-	print_node(n2);
 	printf("\n");
 	struct node* n1 = find_node(n2, 3);
-	print_node(n1);
-	printf("\n");
 	n2 = find_node(n2, 100);
-	print_node(n2);
+
 }
 
 void test_4()
@@ -66,13 +61,28 @@ void test_4()
 		n2 = insert_node(n2, i + (rand() % 10));
 	}
 	n2 = insert_node(n2, 10);
-	print_node(n2);
 	delete_node(n2, 10);
-	printf("\n");
-	print_node(n2);
 	delete_node(n2, 100);
-	printf("\n");
-	print_node(n2);
 }
 
 
+
+void test_code_anhibilator()	
+{	
+	struct node* n2 = create_node(1);	
+	int i;	
+	for (i = 0; i < N; i++)	
+	{	
+		n2 = insert_node(n2, i + (rand() % 10));	
+	}	
+	delete_node(n2, 1);		
+}	
+
+
+void test_Beklemishev_and_Shankov_bizzare_team()	
+{	
+	//значение 16 не удаляется	
+	struct node* n2 = create_node(16);		
+	delete_node(n2, 16);	
+	n2 = insert_node(n2, 22);	
+}
