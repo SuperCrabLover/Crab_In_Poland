20ce68f466391f399eb116fdfd7679578ebed3c9
Poland-Strong <32020828+HCL-271@users.noreply.github.com>
2019-12-15 22:08:49 +0300

    Update Test1.3.c

--- Task1.3/Test1.3.c
+++ Task1.3/Test1.3.c
@@ -11,28 +11,28 @@
 
 void test1() //Базовый тест на работу всех сущесвующих функций
 {
-int a = (rand() % 15) + 1;
-int b = rand() % a + 1;
-int c = rand() % 10 + 1;
-int d = rand() % 5 + 1;
-int x = 0;
-LIST *l = list_new();
-unshift(l, a);
-unshift(l, b);
-push(l, c);
-push(l, d);
-pop(l, &x);
-if (x != d)
-{
-printf("Error");
-exit(-1);
-}
-shift(l, &x);
-if (x != a)
-{
-printf("Error");
-exit(-1);
-}
-reverse(l);
-list_del(l);
+  int a = (rand() % 15) + 1;
+  int b = rand() % a + 1;
+  int c = rand() % 10 + 1;
+  int d = rand() % 5 + 1;
+  int x = 0;
+  LIST *l = list_new();
+  unshift(l, a);
+  unshift(l, b);
+  push(l, c);
+  push(l, d);
+  pop(l, &x);
+  if (x != d)
+  {
+    printf("Error");
+    exit(-1);
+  }
+  shift(l, &x);
+  if (x != a)
+  {
+    printf("Error");
+    exit(-1);
+  }
+  reverse(l);
+  list_del(l);
 }
