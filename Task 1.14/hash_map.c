d3442ab94b724aff07e91c4a566752014e4db4ed
SuperCrabLover <molozhavenko.aa@phystech.edu>
2019-12-15 16:45:01 +0300

    Revert "Merge branch 'Task-1.1-Single-linked-list'"
    This reverts commit 099a966ed42647e34497ce274a8a6ae7d7963293, reversing
    changes made to d341d0654a25f3e0a7473a365316606b91280f3b.

---
+++ Task 1.14/hash_map.c
@@ -0,0 +1,67 @@
+#include "common.h"
+
+unsigned int hash(char *surname)
+{
+	int s = 0;
+	while (*surname)
+	{
+		s += *surname++;
+	}
+	return s % N;
+}
+
+void hash_map_init(void)
+{
+
+}
+
+void hash_map_deinit(void)
+{
+	int i = 0;
+	for (i = 0; i < N; i++)
+	{
+		if (NULL != hash_map[i]) 
+		{
+			list_del(hash_map[i]);
+			hash_map[i] = NULL;
+		}
+	}
+}
+
+void add(char *surname, unsigned long long number)
+{
+	unsigned int index = hash(surname);
+	PERSON node;
+	node.number = number;
+	node.surname = surname;
+	if (NULL == hash_map[index])
+	{
+		hash_map[index] = list_new();
+	}
+	unshift(hash_map[index], node);
+}
+
+LIST *find(char *surname)
+{
+	unsigned int index = hash(surname);
+	if (NULL != hash_map[index])
+	{
+		return find_l(hash_map[index], surname);
+	}
+	return NULL;
+}
+
+
+void del(char *surname)
+{
+	unsigned int index = hash(surname);
+	if (NULL == hash_map[index])
+	{
+		return -1;
+	}
+	if (NULL != hash_map[index])
+	{
+		list_del(hash_map[index]);
+	}
+	hash_map[index] = NULL;
+}
