807a092bb92f7c8221c61c1e902733935389edda
gleb-mak <makarov.ge@phystech.edu>
2019-12-16 12:33:36 +0300

    opa opa opapa
    op davai davai

---
+++ hash-map/hash-map/hash_map.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "h_list.h"
+#define N 16
+
+LIST *hash_map[N];
+
+void hash_map_init(void);
+
+void hash_map_deinit(void);
+
+void add(char *surname, unsigned long long number);
+
+void del(char *surname);
+
+LIST *find(char *surname);
---
+++ hash-map/hash-map/small_test.c
@@ -0,0 +1,40 @@
+#include "common.h"
+#include <conio.h>
+
+int main(void)
+{
+	char s[30];
+	char ch = 0;
+	char *end = "end";
+	char *a = "Ivanov";
+	char *b = "Sidorov";
+	char *c = "Petrov";
+	char *d = "Makarov";
+	char *e = "Tkach";
+	char *f = "Ivanov";
+	unsigned long long a1 = 1234567, b1 = 7654321, c1 = 1357264, d1 = 2641357, e1 = 7531264, f1 = 1326547, s1 = 0;
+	hash_map_init();
+	add(a, a1);
+	add(b, b1);
+	add(c, c1);
+	add(d, d1);
+	add(e, e1);
+	add(f, f1);
+	for (int i = 0; i < N; i++)
+	{
+		print_dl_list(hash_map[i]);
+		printf("\n");
+	}
+	printf("----------------------------------\n");
+	print_dl_list(find("Makarov"));
+	printf("\n----------------------------------\n");
+	del("Ivanov");
+	for (int i = 0; i < N; i++)
+	{
+		print_dl_list(hash_map[i]);
+		printf("\n");
+	}
+	hash_map_deinit();
+	_getch();
+	return 0;
+}
